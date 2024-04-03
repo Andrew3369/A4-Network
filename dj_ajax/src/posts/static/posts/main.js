e0729c80b77c8655eaa947458c52fab27826b7b5
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-02 21:08:13 -0400

    Completed Chapter 7: First Ajax Call
    Used AJAX to manipulate html

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -4,3 +4,19 @@ const helloWorld = document.getElementById('hello-world');
 
 helloWorld.textContent = 'Hello World!';
 helloWorld.innerHTML = '<b>Hello World!<b>';
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response) 
+    {
+        //console.log('success', response)
+        console.log('success', response.text)
+        helloWorld.textContent = response.text
+    },
+    error: function(error)
+    {
+        //console.log('error', error)
+        console.log('error', error.text)
+    },
+})
--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
+    hello_world_view,
     # post_detail,
     # post_update,
     # post_delete,
@@ -10,4 +11,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
+
+    path('hello-world/', hello_world_view, name='hello-world'),
 ]
