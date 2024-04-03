d0604c0c279d89a20d80e356b55df7bbed6a3636
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-02 20:39:14 -0400

    Completed Chapter 6: First DOM Manipulation

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,1 +1,6 @@
-console.log('hello world!')
+console.log('hello world!');
+
+const helloWorld = document.getElementById('hello-world');
+
+helloWorld.textContent = 'Hello World!';
+helloWorld.innerHTML = '<b>Hello World!<b>';
