0d0eb97550622a54153881d1c1523ed931396aa0
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-02 22:02:56 -0400

    Completed Chapter 8: Adding the Spinner
    - From the database, I displayed all 6 posts from ajax and JSON parsing and displayed console.

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log('hello world!');
 
 const helloWorld = document.getElementById('hello-world');
+const postsBox = document.getElementById('posts-box');
 
 helloWorld.textContent = 'Hello World!';
 helloWorld.innerHTML = '<b>Hello World!<b>';
@@ -18,5 +19,27 @@ $.ajax({
     {
         //console.log('error', error)
         console.log('error', error.text)
+    },
+})
+
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response) 
+    {
+        console.log(response)
+        const data = response.data
+        console.log(data)
+        data.forEach(element => {
+            postsBox.innerHTML += `
+                ${element.id} - <b>${element.body}</b><br>
+            `
+            console.log(element)
+        });
+    },
+    error: function(error)
+    {
+        console.log(error)
     },
 })
--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from .views import (
     post_list_and_create,
     hello_world_view,
-    # post_detail,
-    # post_update,
-    # post_delete,
+    load_posts_data_view
+
 )
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
+    path('data/', load_posts_data_view, name='post-data'),
 
     path('hello-world/', hello_world_view, name='hello-world'),
 ]
