f05865a90d4bbef60242652580a98b2f496b3a30
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-04 00:49:45 -0400

    Completed Chapter 13: Like Button with Ajax - Part 2
    Added a like/dislike button that updates the real-time count of how many likes a post has

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls.static import static
 from .views import (
     post_list_and_create,
     hello_world_view,
-    load_posts_data_view
+    load_posts_data_view,
+    like_unlike_post,
 
 )
 
@@ -11,6 +12,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
+    path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('data/<int:num_posts>/', load_posts_data_view, name='post-data'),
 
     path('hello-world/', hello_world_view, name='hello-world'),
--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 from django.core import serializers
 
 # Create your views here.
@@ -29,6 +29,21 @@ def load_posts_data_view(request, num_posts):
         }
         data.append(item)
     return JsonResponse({'data': data[lower:upper], 'size': size})
+
+def like_unlike_post(request):
+    #if request.is_ajax(): this didnt work
+    #if request.method == 'POST': this also didnt work
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
 
 
 def hello_world_view(request):
