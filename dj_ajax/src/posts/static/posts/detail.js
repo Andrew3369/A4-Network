2728035312ad6aff244cdacab887bb248ec0cf6b
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-04 15:52:04 -0400

    Completed Chapter 18: Working on the Post Detail Page - Part 1
    Added buttons from bootstrap
     Back functionaility

---
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -0,0 +1,10 @@
+console.log("hello world detail")
+
+const backBtn = document.getElementById('back-btn')
+// const deleteBtn = document.getElementById('delete-btn')
+// const updateBtn = document.getElementById('update-btn')
+
+backBtn.addEventListener('click', ()=>
+{
+    history.back()
+})
