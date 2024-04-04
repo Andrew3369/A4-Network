31263d525c5cd356fd14272f14dfa7042539efbf
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-04 18:41:40 -0400

    Completed Chapter 21: Working on the Post Detail Page - Part 3
    hid 2 buttons (update and delete)
    depending on what user is logged in, they can delete their own posts

--- dj_ajax/src/posts/static/posts/detail.js
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -1,8 +1,8 @@
 console.log("hello world detail")
 
 const backBtn = document.getElementById('back-btn')
-// const deleteBtn = document.getElementById('delete-btn')
-// const updateBtn = document.getElementById('update-btn')
+const deleteBtn = document.getElementById('delete-btn')
+const updateBtn = document.getElementById('update-btn')
 
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
@@ -18,6 +18,19 @@ $.ajax({
     success: function(response)
     {
         console.log(response)
+        const data = response.data
+
+        if (data.logged_in !== data.author)
+        {
+            console.log('differnet')
+        }
+        else
+        {
+            console.log('same')
+            updateBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
+
         spinnerBox.classList.add('not-visible')
     },
     error: function(error)
