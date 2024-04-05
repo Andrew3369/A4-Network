a264c60c7fdc733e6c970b4ec5abb1dac486be33
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-05 01:54:59 -0400

    Completed Chapter 24: Writing Update and Delete Views
    Now able to delete and update posts with new information, or removing information

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.conf.urls.static import static
 from .views import (
     post_list_and_create,
-    hello_world_view,
     load_posts_data_view,
     like_unlike_post,
     post_detail,
     post_detail_data,
+    delete_post,
+    update_post,
 
 )
 
@@ -16,6 +17,8 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
+    path('<pk>/update/', update_post, name='post-update'),
+    path('<pk>/delete/', delete_post, name='post-delete'),
     
     path('data/<int:num_posts>/', load_posts_data_view, name='post-data'),
     path('<pk>/data/', post_detail_data, name='post-detail-data'),
