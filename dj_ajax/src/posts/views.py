6063a3d723e0cf3b7b2fb05f1bede0b360a8a66b
Andrew3369 <andrewjanosbabos@gmail.com>
2024-04-05 02:11:23 -0400

    Completed Chapter 25: Update Post with Ajax
    - Handled error and condition checking for when the post is updated, changes the title or body when update changes have been confirmed
    - Post is now updated in the backend server in the database

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -98,13 +98,13 @@ def update_post(request, pk):
         obj.title = new_title
         obj.body = new_body
         obj.save()
-    return JsonResponse({
-        'title': new_title,
-        'body': new_body,
-    })
+        return JsonResponse({
+            'title': new_title,
+            'body': new_body,
+        })
 
 def delete_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.headers.get('x-requested-with') == 'XMLHttpRequest': # equivalent to request.is_ajax()
         obj.delete()
-    return JsonResponse({})
+        return JsonResponse({})
